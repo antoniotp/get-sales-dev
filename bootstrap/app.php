b03a164396907062bf3365291717e9e50c67ae7b
Crisanto González Mora <cgonzalez@jobomas.com>
2025-06-10 18:27:25 -0600

    Add CSRF exception for webhook routes

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -21,6 +21,9 @@ return Application::configure(basePath: dirname(__DIR__))
             HandleInertiaRequests::class,
             AddLinkHeadersForPreloadedAssets::class,
         ]);
+        $middleware->validateCsrfTokens(except: [
+            'webhook/*',
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //

62f79c468c3321557f7b0b329c7d4926c27a82ed
Crisanto González Mora <cgonzalez@jobomas.com>
2025-07-31 13:57:37 -0600

    feat: add a default layout for app content rendering

---
+++ resources/js/layouts/app/app-content-default-layout.tsx
@@ -0,0 +1,14 @@
+import { type PropsWithChildren } from 'react'
+
+export default function AppContentDefaultLayout({ children }: PropsWithChildren) {
+    // When server-side rendering, we only render the layout on the client...
+    if (typeof window === 'undefined') {
+        return null;
+    }
+
+    return (
+        <div className="flex h-full flex-1 flex-col px-4 py-6 rounded-xl overflow-hidden dark:bg-gray-900">
+            {children}
+        </div>
+    )
+}

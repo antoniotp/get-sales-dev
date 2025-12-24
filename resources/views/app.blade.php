4e7d8c1d7ae508220eecc180174b67e15111c4fa
Crisanto González Mora <cgonzalez@jobomas.com>
2025-12-23 22:43:22 -0600

    feat(pwa): add web app manifest and required icons
    - Create manifest.json to define PWA properties.
    - Link manifest in-app layout for 'Add to Home Screen'.
    - Add missing android-chrome icon assets required by manifest.
    - Replace the old logo with the newest.

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -36,6 +36,13 @@
         <link rel="icon" href="/favicon.svg" type="image/svg+xml">
         <link rel="apple-touch-icon" href="/apple-touch-icon.png">
 
+        <link rel="icon" type="image/png" href="/favicon-96x96.png" sizes="96x96" />
+        <link rel="shortcut icon" href="/favicon.ico" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+        <meta name="apple-mobile-web-app-title" content="Get-Sales" />
+
+        <link rel="manifest" href="/manifest.json">
+
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600" rel="stylesheet" />
 

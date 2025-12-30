7dc784b82297635c3d8127fadd651b8191876d27
Crisanto González Mora <cgonzalez@jobomas.com>
2025-12-29 22:18:59 -0600

    feat(notifications): delete expired push subscriptions on failure

--- app/Listeners/Notification/HandleFailedPushNotification.php
+++ app/Listeners/Notification/HandleFailedPushNotification.php
@@ -2,6 +2,7 @@
 
 namespace App\Listeners\Notification;
 
+use App\Models\UserPushSubscription;
 use Illuminate\Support\Facades\Log;
 use NotificationChannels\WebPush\Events\NotificationFailed;
 
@@ -29,5 +30,12 @@ class HandleFailedPushNotification
             'status_code' => $report?->getResponse()?->getStatusCode(),
             'response_body' => $report?->getResponse()?->getBody()->getContents(),
         ]);
+
+        // If the subscription is expired, delete it from the database.
+        if ($report && $report->isSubscriptionExpired()) {
+            $endpoint = $report->getEndpoint();
+            Log::info('Deleting expired push subscription.', ['endpoint' => $endpoint]);
+            UserPushSubscription::where('endpoint', $endpoint)->delete();
+        }
     }
 }

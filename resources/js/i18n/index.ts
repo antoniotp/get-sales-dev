7a7bfadabb86cfd02869fef97da25693c055f987
José Antonio Tlalpa Potrero <antonio@MacBook-Air-de-Jose.local>
2026-02-12 21:07:28 -0600

    feat(i18n): Add translations for register, chatbot and contacts components

--- resources/js/i18n/index.ts
+++ resources/js/i18n/index.ts
@@ -7,8 +7,12 @@ import enNavigation from './locales/en/navigation.json';
 import enHome from './locales/en/home.json';
 import esLogin from './locales/es/login.json';
 import enLogin from './locales/en/login.json';
+import esRegister from './locales/es/register.json';
+import enRegister from './locales/en/register.json';
 import esChatbot from './locales/es/chatbot.json';
 import enChatbot from './locales/en/chatbot.json';
+import esContact from './locales/es/contact.json';
+import enContact from './locales/en/contact.json';
 
 i18n
   .use(initReactI18next)
@@ -18,13 +22,17 @@ i18n
         navigation: esNavigation,
         home: esHome,
         login: esLogin,
+        register: esRegister,
         chatbot: esChatbot,
+        contact: esContact,
       },
       en: {
         navigation: enNavigation,
         home: enHome,
         login: enLogin,
+        register: enRegister,
         chatbot: enChatbot,
+        contact: enContact,
       },
     },
     lng: 'es',
